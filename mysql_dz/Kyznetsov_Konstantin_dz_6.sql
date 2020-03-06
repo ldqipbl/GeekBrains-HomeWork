1ee1e0d7e94d2ec37cca87c7e704d7990abc5816
dqpb <dqpb@localhost.localdomain>
2020-03-06 13:18:39 +0700

    Исправил 1 задание в Kyznetsov_Konstantin_dz_6.sql

--- mysql_dz/Kyznetsov_Konstantin_dz_6.sql
+++ mysql_dz/Kyznetsov_Konstantin_dz_6.sql
@@ -6,16 +6,26 @@
 	Определить кто больше поставил лайков (всего) - мужчины или женщины?
 */
 
+--      Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим пользователем.
 
---        Подсчитать общее количество лайков, которые получили пользователи младше 10 лет..
+SELECT count(*) AS max_num FROM media WHERE 
+	user_id IN (
+		SELECT initiator_user_id FROM friend_requests WHERE (target_user_id = 10) AND status='approved' 
+		union 
+		SELECT target_user_id FROM friend_requests WHERE (initiator_user_id = 10) AND status='approved') 
+	GROUP BY user_id 
+	ORDER BY max_num DESC LIMIT 1;
+
+
+--      Подсчитать общее количество лайков, которые получили пользователи младше 10 лет..
 
 SELECT count(*) FROM likes WHERE (SELECT birthday FROM profiles WHERE likes.user_id = user_id) > NOW() - INTERVAL 18 year;
 
 
---        Определить кто больше поставил лайков (всего) - мужчины или женщины?
+--      Определить кто больше поставил лайков (всего) - мужчины или женщины?
 --		TODO сравнить.
 
-SELECT 
-	(SELECT count(*) FROM likes WHERE (SELECT gender FROM profiles WHERE likes.user_id = user_id) = 'm') as male,
-       	(SELECT count(*) FROM likes WHERE (SELECT gender FROM profiles WHERE likes.user_id = user_id) = 'f') as female;
+ SELECT
+        (SELECT count(*) FROM likes WHERE (SELECT gender FROM profiles WHERE likes.user_id = user_id) = 'm') as male,
+        (SELECT count(*) FROM likes WHERE (SELECT gender FROM profiles WHERE likes.user_id = user_id) = 'f') as female;
 
