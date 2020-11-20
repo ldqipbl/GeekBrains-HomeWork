2eda100c953760b95f63772142b4f378c134c55f
dqpb <dqpb@localhost.localdomain>
2020-11-20 15:08:28 +0700

    lesson_6. Query renew

--- django_2_dz/geekshop/geekshop/ordersapp/views.py
+++ django_2_dz/geekshop/geekshop/ordersapp/views.py
@@ -12,9 +12,10 @@ from basketapp.models import Basket
 from mainapp.models import Product
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class OrderList(ListView):
+class OrderList(LoginRequiredMixin, ListView):
     model = Order
 
     def get_queryset(self):
@@ -84,10 +85,12 @@ class OrderItemsUpdate(UpdateView):
     def get_context_data(self, **kwargs):
         data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
         if self.request.POST:
             data["orderitems"] = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(instance=self.object)
+            queryset = self.object.orderitems.select_related()
+            formset = OrderFormSet(instance=self.object, queryset=queryset)
             for form in formset.forms:
                 if form.instance.pk:
                     form.initial["price"] = form.instance.product.price
