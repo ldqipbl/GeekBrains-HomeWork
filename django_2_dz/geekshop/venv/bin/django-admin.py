b3f0614747e39adfc6314ab20929871a83930e4b
dqpb <dqpb@localhost.localdomain>
2020-11-29 06:19:11 +0700

    -venv

--- django_2_dz/geekshop/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dqpb/my_doc/Python/geekBrein/GeekBrains-HomeWork/django_2_dz/geekshop/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
