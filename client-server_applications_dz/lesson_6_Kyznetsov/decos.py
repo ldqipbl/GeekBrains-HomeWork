5be7bdf50419788f5844a3c7527d5552bcff137b
dqpb <dqpb@localhost.localdomain>
2020-12-25 06:54:50 +0700

    update lessen_6

--- client-server_applications_dz/lesson_6_Kyznetsov/decos.py
+++ client-server_applications_dz/lesson_6_Kyznetsov/decos.py
@@ -3,6 +3,7 @@ import logging
 import log.config_server_log
 import log.config_client_log
 import inspect
+import traceback
 
 
 if sys.argv[0].find('client') == -1:
@@ -16,25 +17,11 @@ def log(func_to_log):
         LOGGER.debug(
             f"Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}.\n"
             f"Вызов из модуля {func_to_log.__module__}.\n"
-            f"Вызов из функции {inspect.stack()[1][3]}\n"
+            f"Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}\n"
+            f"Вызов из функции {inspect.stack()[1][3]}\n", 
+            stacklevel=2
+
         )
         return func_to_log(*args, **kwargs)
     return log_saver
-
-
-'''
-# Реализация в виде класса
-class Log:
-    """Класс-декоратор"""
-    def __call__(self, func_to_log):
-        def log_saver(*args, **kwargs):
-            """Обертка"""
-            ret = func_to_log(*args, **kwargs)
-            LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
-                         f'Вызов из модуля {func_to_log.__module__}. Вызов из'
-                         # f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
-                         f'Вызов из функции {inspect.stack()[1][3]}')
-            return ret
-        return log_saver
-'''
 
